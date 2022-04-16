957e2e9ba088036dd977726581662ad2f2e449e1
guosj <515878133@qq.com>
2022-04-16 09:52:32 +0800

    update myecho.c

--- FileRW/myecho.c
+++ FileRW/myecho.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-#define MAXLEN  1024
 
 int main(int argc, char *argv[])
 {
-    /* command arguments */
     while (--argc)
         printf("%s ", *(++argv));
     printf("\n");
